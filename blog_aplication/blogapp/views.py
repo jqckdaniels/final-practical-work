8a35ed6f11bad5ebad7b2c200f02fcfef86a60da
jqckdaniels <martynaskarveckas@gmail.com>
2022-12-09 10:47:58 +0200

    fourth commit

--- blog_aplication/blogapp/views.py
+++ blog_aplication/blogapp/views.py
@@ -1,8 +1,25 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, redirect
+from .forms import CommentForm
+from .models import Post, Category
 
-from .models import Post
-
-def detail(request, slug):
+def detail(request, category_slug, slug):
     post = get_object_or_404(Post, slug=slug)
 
-    return render(request, 'blogapp/detail.html', {'post': post})
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+
+            return redirect('post_detail', slug=slug)
+    else:
+        form = CommentForm()
+
+    return render(request, 'blogapp/detail.html', {'post': post, 'form': form})
+
+def category(request, slug):
+    category = get_object_or_404(Category, slug=slug)
+
+    return render(request, 'blogapp/category.html',{ 'category': category})
