3ac89c8828e21f0bf4403597271cdbc44e20ff24
jqckdaniels <martynaskarveckas@gmail.com>
2022-12-11 14:04:40 +0200

    fifth commit

--- blog_aplication/blog/views.py
+++ blog_aplication/blog/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from blogapp.models import Post
 
 def frontpage(request):
-    posts = Post.objects.all()
+    posts = Post.objects.filter(status=Post.ACTIVE)
     return render(request, 'blog/frontpage.html', {'posts': posts})
 
 def about(request):
--- blog_aplication/blogapp/views.py
+++ blog_aplication/blogapp/views.py
@@ -3,7 +3,7 @@ from .forms import CommentForm
 from .models import Post, Category
 
 def detail(request, category_slug, slug):
-    post = get_object_or_404(Post, slug=slug)
+    post = get_object_or_404(Post, slug=slug, status=Post.ACTIVE)
 
     if request.method == 'POST':
         form = CommentForm(request.POST)
@@ -23,3 +23,10 @@ def category(request, slug):
     category = get_object_or_404(Category, slug=slug)
 
     return render(request, 'blogapp/category.html',{ 'category': category})
+
+def search(request):
+    query = request.GET.get('query', '')
+
+    posts = Post.objects.filter(title_icontains=query)
+
+    return render(request, 'blogapp/search.html', {'posts': posts, 'query': query})
